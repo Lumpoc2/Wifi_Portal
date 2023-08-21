1cbf7157de676f2778c3c1684291dbf4318b5a28
ZedUnderson <lasotrec@gmail.com>
2023-08-21 12:07:46 +0800

    Add Login for front-end

--- index.php
+++ index.php
@@ -1,40 +1,37 @@
-<?php
-function getMacAddress() {
-    $macAddress = '';
-    
-    $osType = strtoupper(substr(PHP_OS, 0, 3));
-    
-    if ($osType === 'WIN') {
-        // Windows
-        $command = 'ipconfig /all';
-        ob_start();
-        passthru($command);
-        $output = ob_get_clean();
-        
-        $pattern = '/Physical\s+Address\s+:\s+([0-9A-Fa-f\-:]+)/';
-        if (preg_match($pattern, $output, $matches)) {
-            $macAddress = str_replace('-', ':', $matches[1]);
-        }
-    } elseif ($osType === 'LIN') {
-        // Linux
-        $command = '/sbin/ifconfig';
-        ob_start();
-        passthru($command);
-        $output = ob_get_clean();
-        
-        $pattern = '/([0-9A-Fa-f\-:]{17})/';
-        if (preg_match($pattern, $output, $matches)) {
-            $macAddress = $matches[1];
-        }
-    } else {
-        $macAddress = 'Unsupported OS';
-    }
-    
-    return $macAddress;
-}
+<!DOCTYPE html>
+<html lang="en">
 
-$macAddress = getMacAddress();
-echo "MAC Address: " . $macAddress;
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
+</head>
 
-echo "THIS IS TEST"
-?>
+<body>
+    <section class="py-4 py-xl-5">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-5">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-person">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
+                                </svg></div>
+                            <form class="text-center" method="post">
+                                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                                <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                                <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                                <p class="text-muted">Forgot your password?</p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
